bc36f2573f253bdd182ccebf5f39ecab8db186b5
Iuliia Tsentylo <labliuk.maksym@gmail.com>
2020-02-11 18:32:37 +0200

    Updates

---
+++ precache-manifest.911442cffbf2268282f1817dce1c06e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc966c7bfc0c3a709c9e9eb14b7656c1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "864652386f0e0c33b4c7",
+    "url": "./static/css/main.49c6268b.chunk.css"
+  },
+  {
+    "revision": "ebf091a2f49af72b4477",
+    "url": "./static/js/2.c7ea46c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.c7ea46c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "864652386f0e0c33b4c7",
+    "url": "./static/js/main.b0c92be9.chunk.js"
+  },
+  {
+    "revision": "0005f2a639f1868a7e30",
+    "url": "./static/js/runtime-main.ef9b1eb6.js"
+  }
+]);
